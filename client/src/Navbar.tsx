c51aaf47a2301276bf0b3884a418bfdc22f70a61
mpimble <pimblemichael@yahoo.com>
2025-08-04 11:44:50 -0400

    Updated Navbar with new links

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -4,11 +4,11 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="navbar-title"><Link to='/'>Personal Website</Link></div>
+            <div className="navbar-title"><Link to='/'>Pim's Regression Tool</Link></div>
             <ul className="nav-links">
-                <li><Link to='/'>Login</Link></li>
-                <li><Link to='/about'>Dashboard</Link></li>
-                <li><Link to='/projects'>Account</Link></li>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/dashboard'>Dashboard</Link></li>
+                <li><Link to='/account'>Account</Link></li>
             </ul>
         </nav>
     )
