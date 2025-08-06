902353cf81475a069af9bcc628bf8e953bf6bf03
mpimble <pimblemichael@yahoo.com>
2025-08-06 00:05:48 -0400

    Changed Navbar Title

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -4,7 +4,7 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="navbar-title"><Link to='/'>Pim's Regression Tool</Link></div>
+            <div className="navbar-title"><Link to='/'>Michael's Regression Tool</Link></div>
             <ul className="nav-links">
                 <li><Link to='/login'>Login</Link></li>
                 <li><Link to='/dashboard'>Dashboard</Link></li>
