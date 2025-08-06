ff472014b8384b7e46e52564468a514416d2ed77
mpimble <pimblemichael@yahoo.com>
2025-08-06 00:07:24 -0400

    Made a custom homepage that changes depending on login status

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from 'react-router-dom'
+import { isAuthenticated } from './auth'
+import { getFullName } from './userinfo'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fullName, setFullName] = useState("");
+
+  useEffect(() => {
+    getFullName().then(result => {
+      setFullName(result);
+    });
+  }, []);
+
+  if (isAuthenticated()) {
+    return (
+      <div>
+        <h2>Welcome, {fullName}!</h2>
+        <p>Head to the <Link to="/dashboard">Dashboard</Link> to create and view your regression projects.</p>
+      </div>
+    );
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <p>The home page for my custom regression tool. <Link to="login">Login</Link> to create and view your regression projects.</p>
+    </div>
+  );
 }
 
 export default App
