cefda36ef1f8ed9d298c8787f878c277344724e6
mpimble <pimblemichael@yahoo.com>
2025-09-01 17:19:56 -0400

    Made the navbar display "Login" or "Logout" accordingly

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom"
 import './Navbar.css'
+import { isAuthenticated } from "./auth";
 
-const Navbar = () => {
+function Navbar() {
+    let lioLink: string = '/login';
+    let lioText: string = 'Login';
+
+    if (isAuthenticated()) {
+        lioLink = '/logout';
+        lioText = 'Logout';
+    }
+
     return (
         <nav className="navbar">
             <div className="navbar-title"><Link to='/'>Michael's Regression Tool</Link></div>
             <ul className="nav-links">
-                <li><Link to='/login'>Login</Link></li>
+                <li><Link to={lioLink}>{lioText}</Link></li>
                 <li><Link to='/dashboard'>Dashboard</Link></li>
                 <li><Link to='/account'>Account</Link></li>
             </ul>
