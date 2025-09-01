bdc0bee391e3559f97c63f040ec36ca6c5ad4d5c
mpimble <pimblemichael@yahoo.com>
2025-09-01 17:16:10 -0400

    Put content on 'Account' page

--- client/src/AccountPage.tsx
+++ client/src/AccountPage.tsx
@@ -1,6 +1,34 @@
-const AccountPage = () => {
+import { useState, useEffect } from "react";
+import { getEmail, getFullName, getUsername } from "./userinfo";
+
+function AccountPage() {
+    const [fullName, setFullName] = useState("");
+    useEffect(() => {
+        getFullName().then(result => {
+            setFullName(result);
+        });
+    }, []);
+
+    const [email, setEmail] = useState("");
+    useEffect(() => {
+        getEmail().then(result => {
+            setEmail(result);
+        });
+    }, []);
+
+    const [username, setUsername] = useState("");
+    useEffect(() => {
+        getUsername().then(result => {
+            setUsername(result);
+        });
+    }, []);
+
     return (
-        <div></div>
+        <div>
+            <h3>Your username: {username}</h3>
+            <h3>Your full name: {fullName}</h3>
+            <h3>Your email address: {email}</h3>
+        </div>
     )
 }
 
